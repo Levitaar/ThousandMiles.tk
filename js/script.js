bf9798a376e65cdb29832a60617e8ffe9475c2a4
Luis Huiza <huizl151@newschool.edu>
2016-10-06 22:28:25 +0000

    setup

--- js/script.js
+++
@@ -1,7 +0,0 @@
-var degrees = 0;
-
-$(".crystal").each(function() {
-    degrees += 180/$(".crystal").length;
-    var rotation = "rotate(" + degrees + "deg)";
-   $(this).css("transform", rotation);
-});
